6e943b54ad3a1ce678edb880961b9735e9c11ba2
Fikri Prayoga <fikri@mamiteam.com>
2020-01-25 01:31:04 +0700

    Final

--- FootballMatchSchedule/app/src/main/java/id/co/blogspot/fikriprayoga/www/footballmatchschedule/frontendcontroller/searchteam/SearchTeamView.kt
+++
@@ -1,8 +0,0 @@
-package id.co.blogspot.fikriprayoga.www.footballmatchschedule.frontendcontroller.searchteam
-
-interface SearchTeamView {
-    fun onFailure(searchTeamModel: SearchTeamModel)
-
-    fun onResponse(searchTeamModel: SearchTeamModel)
-
-}
